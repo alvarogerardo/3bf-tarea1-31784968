bf6ff560a2d7372bf03319906482bb336382178b
alvarogerardo <92186118+alvarogerardo@users.noreply.github.com>
2021-10-15 18:42:31 -0300

    Add files via upload

---
+++ tarea-1.php
@@ -0,0 +1,16 @@
+<?php 
+echo "Imprimir n&uacutemeros del cero al 10 con bucle for.<br><br>";
+
+for ($i=0; $i <=10 ; $i++) { 
+	echo "Iteraci&oacuten " . $i+1 . " >>>> $i.<br>";
+}
+
+echo "<br><br>Y con bucle while...<br><br>";
+	
+	$variable=0;
+
+	while ($variable<=10) {
+		echo "Iteraci&oacuten " . $variable+1 . " >>>> $variable.<br>";
+		$variable++;
+	}
+ ?>
